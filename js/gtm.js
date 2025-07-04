2db5ee910f720043b9f3e0932570894a7b1fa609
e <motusamotusrblx@gmail.com>
2025-07-04 16:07:41 +0800

    some seo fixing
    some content added
    added sitemap.xml

--- js/gtm.js
+++ js/gtm.js
@@ -1,17 +1,28 @@
-import config from './config.js';
-
-// GTM initialization
 export function initGTM() {
-    // Update noscript iframe
-    const noscriptIframe = document.querySelector('noscript iframe[data-gtm-src]');
-    if (noscriptIframe) {
-        noscriptIframe.src = noscriptIframe.getAttribute('data-gtm-src') + config.gtmId;
+    const gtmId = window.ENV_GTM_ID;
+    if (!gtmId) {
+        console.error('GTM ID not found');
+        return;
     }
 
-    // Initialize GTM
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl= 'dataLayer' != l?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer',config.gtmId);
+    try {
+        const noscriptIframe = document.querySelector('noscript iframe[data-gtm-src]');
+        if (noscriptIframe) {
+            noscriptIframe.src = noscriptIframe.getAttribute('data-gtm-src') + gtmId;
+        }
+
+        // Initialize GTM
+        (function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = 'dataLayer' != l ? '&l=' + l : '';
+            j.async = true;
+            j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', gtmId);
+    } catch (error) {
+        console.error('Failed to initialize GTM:', error);
+    }
 }
