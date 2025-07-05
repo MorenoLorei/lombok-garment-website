289201208b5e1c5aa66800574221185c5a5d45d6
e <motusamotusrblx@gmail.com>
2025-07-05 16:31:55 +0800

    some changes

--- js/gtm.js
+++ js/gtm.js
@@ -1,5 +1,5 @@
 export function initGTM() {
-  const gtmId = process.env.GTM_ID;
+  const gtmId = 'GTM-P9P76JBD';
   if (!gtmId) {
     console.error('GTM ID not found');
     return;
@@ -16,8 +16,8 @@ export function initGTM() {
       w[l] = w[l] || [];
       w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
       var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s),
-        dl = 'dataLayer' != l ? '&l=' + l : '';
+          j = d.createElement(s),
+          dl = 'dataLayer' != l ? '&l=' + l : '';
       j.async = true;
       j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
       f.parentNode.insertBefore(j, f);
